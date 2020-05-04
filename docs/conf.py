12eb3565f96579b4fe78c74e9ae11a471c152854
luecks@gmail.com <Snowmitko.12>
2020-05-04 09:52:13 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,8 @@ release = '0.19'
 extensions = ['sphinx.ext.autodoc'
 ]
 
+master_doc = 'index'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
