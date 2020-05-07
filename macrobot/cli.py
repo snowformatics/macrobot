7d8bcd6f8c94938fac0e06a0ea58f037aeae71f4
luecks@gmail.com <Snowmitko.12>
2020-05-07 09:18:53 +0200

    small bug fixes

--- macrobot/cli.py
+++ macrobot/cli.py
@@ -46,7 +46,9 @@ def main():
     # Source image path
     source_path = args.source_path
     if source_path == 'test_images':
-        source_path = os.path.join(os.path.dirname(os.getcwd()), 'macrobot', 'data')
+        #source_path = os.path.join(os.path.dirname(os.getcwd()), 'macrobot', 'data')
+        source_path = data_path
+
     print (source_path)
     # Path to store the results
     destination_path = args.destination_path
