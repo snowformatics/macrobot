8d7d7d04f38089518bfb274fd2edade8dfb0c846
luecks@gmail.com <Snowmitko.12>
2024-12-09 15:14:37 +0100

    refactored for different hardware setups

--- macrobot/data_managment.py
+++ macrobot/data_managment.py
@@ -63,69 +63,75 @@ def copy_and_verify(path1, path2):
 
 
 def process_and_merge_files(excel_file, csv_file, identifier, output_file):
-    try:
-        # Extract the first word after splitting the filenames by '_'
-        excel_base = os.path.basename(excel_file).split('_')[0]
-        csv_base = os.path.basename(csv_file).split('_')[0]
-        #print (excel_file, csv_file, output_file)
-        if csv_base.startswith('gb2'):
-            csv_base = csv_base + '_' + os.path.basename(csv_file).split('_')[1]
-        print (csv_base)
 
-        # Check if both files share the same starting word
-        # if excel_base != csv_base:
-        #     raise ValueError(
-        #         f"Filename mismatch: '{excel_file}' and '{csv_file}' do not start with the same word after split by '_'."
-        #     )
-        # Read the Excel file
-        #df1 = pd.read_excel(excel_file)
-        df1 = pd.read_csv(excel_file, delimiter='\t')
-        #df1 = df1.rename(columns={'plate_id': 'INFECTION', 'Lane_ID': 'LANE_ID2', 'Plate_ID': 'PLATE_ID2'})
+    # Extract the first word after splitting the filenames by '_'
+    excel_base = os.path.basename(excel_file).split('_')[0]
+    csv_base = os.path.basename(csv_file).split('_')[0]
+    #print (csv_base)
+    if csv_base.startswith('gb2'):
+        csv_base = csv_base + '_' + os.path.basename(csv_file).split('_')[1]
 
-        # Read the CSV file
-        df2 = pd.read_csv(csv_file, delimiter=';')
-       # print (df2)
+    if output_file is None:
+        output_file = r"\\psg-09\Mikroskop\Images\hsm_db\\" + csv_base + '_db.csv'
+    #print (output_file)
+    # Check if both files share the same starting word
+    # if excel_base != csv_base:
+    #     raise ValueError(
+    #         f"Filename mismatch: '{excel_file}' and '{csv_file}' do not start with the same word after split by '_'."
+    #     )
+    # Read the Excel file
+    #df1 = pd.read_excel(excel_file)
+    df1 = pd.read_csv(excel_file, delimiter='\t')
+    df1 = df1.drop(columns=['dai'], errors='ignore')
+    #df1 = df1.rename(columns={'plate_id': 'INFECTION', 'Lane_ID': 'LANE_ID2', 'Plate_ID': 'PLATE_ID2'})
 
-        # Create the `plate_index` column by splitting `Plate_ID` and taking the last entry
-        df2['plate_index'] = df2['Plate_ID'].apply(lambda x: x.split('_')[-1]) + '-' + df2['Lane_ID'].astype(str)
+    # Read the CSV file
+    df2 = pd.read_csv(csv_file, delimiter=';')
+    print (df2)
 
-        # Check if both DataFrames contain the identifier column
-        if identifier not in df1.columns:
-            raise ValueError(f"Identifier column '{identifier}' is missing in the Excel file.")
-        if identifier not in df2.columns:
-            raise ValueError(f"Identifier column '{identifier}' is missing in the CSV file.")
+    # Create the `plate_index` column by splitting `Plate_ID` and taking the last entry
+    #df2['plate_index'] = df2['Plate_ID'].apply(lambda x: x.split('_')[-1]) + '-' + df2['Lane_ID'].astype(str)
+    df2['plate_index'] = df2['expNr'].astype(str) + '-' + df2['Plate_ID'].apply(lambda x: x.split('_')[-1]) + '-' + df2['Lane_ID'].astype(str)
+    df1['plate_index'] = df1['experiment'].astype(str) + '-' + df1['plate_id'] + '-' + df1['lane_id'].astype(str)
+    #print (df1['plate_index'])
+    #print(df2['plate_index'])
+    # Check if both DataFrames contain the identifier column
+    if identifier not in df1.columns:
+        raise ValueError(f"Identifier column '{identifier}' is missing in the Excel file.")
+    if identifier not in df2.columns:
+        raise ValueError(f"Identifier column '{identifier}' is missing in the CSV file.")
 
-        # Merge the files on the specified identifier
-        merged_df = pd.merge(df1, df2, on=identifier, how='outer')
-        merged_df = merged_df.drop(columns=['expNr', 'barcode'])
-        # Add a new column `type` with the value `macrobot`
-        merged_df['type'] = 'macrobot'
-        # Add a new column `url1` with the path `\\hsm\AGR-BIM\macrobot\ + csv_base`
-        merged_df['url1'] = merged_df.apply(
-            lambda row: f"\\\\hsm\\AGR-BIM\\macrobot\\{row['experiment']}\\{row['dai']}dai", axis=1
+    # Merge the files on the specified identifier
+    merged_df = pd.merge(df1, df2, on=identifier, how='inner')
+    merged_df = merged_df.drop(columns=['expNr', 'barcode'], errors='ignore')
+    #print (df1['plate_index'])
+    # Add a new column `type` with the value `macrobot`
+    merged_df['type'] = 'macrobot'
+    # Add a new column `url1` with the path `\\hsm\AGR-BIM\macrobot\ + csv_base`
+    merged_df['url1'] = merged_df.apply(
+        lambda row: f"\\\\hsm\\AGR-BIM\\macrobot\\{row['experiment']}\\{row['dai']}", axis=1
+    )
+    merged_df['url2'] = merged_df.apply(
+        lambda row: f"\\\\hsm\\AGR-BIM\\Results\\BluVisionMacro\\{row['experiment']}\\{row['dai']}", axis=1
+    )
+    # Validate the merged DataFrame
+    if len(merged_df.columns) < len(df2.columns):
+        raise ValueError("Merged DataFrame does not contain at least as many columns as the CSV file.")
+    non_na_percentage = merged_df.notna().sum().sum() / merged_df.size
+    if non_na_percentage <= 0.1:
+        raise ValueError(
+            f"Less than 10% of the cells in the merged DataFrame are non-NA or non-empty. "
+            f"Non-NA percentage: {non_na_percentage:.2%}"
         )
-        merged_df['url2'] = merged_df.apply(
-            lambda row: f"\\\\hsm\\AGR-BIM\\Results\\BluVisionMacro\\{row['experiment']}\\{row['dai']}dai", axis=1
-        )
+    merged_df = merged_df.rename(columns={'%_Inf': 'INFECTION', 'Lane_ID': 'LANE_ID2', 'Plate_ID': 'PLATE_ID2',
+                                          'index': 'index_', 'user': 'user_'})
+    merged_df['experiment'] = csv_base
+    # Save the merged DataFrame to a new CSV file
+    merged_df.to_csv(output_file, index=False, sep=";")
+    #print(f"Merged file saved to: {output_file}")
 
-        # Validate the merged DataFrame
-        if len(merged_df.columns) < len(df2.columns):
-            raise ValueError("Merged DataFrame does not contain at least as many columns as the CSV file.")
-        non_na_percentage = merged_df.notna().sum().sum() / merged_df.size
-        if non_na_percentage <= 0.1:
-            raise ValueError(
-                f"Less than 10% of the cells in the merged DataFrame are non-NA or non-empty. "
-                f"Non-NA percentage: {non_na_percentage:.2%}"
-            )
-        merged_df = merged_df.rename(columns={'%_Inf': 'INFECTION', 'Lane_ID': 'LANE_ID2', 'Plate_ID': 'PLATE_ID2',
-                                              'index': 'index_', 'user': 'user_'})
-        merged_df['experiment'] = csv_base
-        # Save the merged DataFrame to a new CSV file
-        merged_df.to_csv(output_file, index=False)
-        #print(f"Merged file saved to: {output_file}")
 
-    except Exception as e:
-        print(f"An error occurred: {e}")
+
 
 
 
@@ -159,12 +165,13 @@ def export_db(csv_file_path):
     db_headers = [x[0] for x in cursor.description]
 
     # Load CSV file
-    csv_data = pd.read_csv(csv_file_path, delimiter=',')
+    csv_data = pd.read_csv(csv_file_path, delimiter=';')
     csv_data.columns = [col.upper() for col in csv_data.columns]  # Convert CSV headers to uppercase
+    csv_data['DAI'] = csv_data['DAI'].str.replace('dai', '')
 
 
     #csv_data = csv_data.where(pd.notnull(csv_data), None)
-    #csv_data = csv_data.fillna('NA')
+    csv_data = csv_data.fillna('NA')
     # Format date columns
     date_columns = ['SAW_DATE', 'INOC_DATE']
     for col in date_columns:
@@ -246,11 +253,6 @@ def export_db(csv_file_path):
     cursor.close()
     db_con.close()
   
-
-#process_and_merge_files("meta_empty.csv", "MB0267_leaf.csv", "plate_index", "MB0267_db.csv")
-#export_db("C:/Users/lueck/PycharmProjects/macrobot/macrobot/MB0267_db.csv")
-#copy_and_verify("//psg-09/Mikroskop/Images/BluVisionMacro/MB0263/6dai/", "//hsm/AGR-BIM/Results/BluVisionMacro/MB0263/6dai/")
-
 def old_mb_data():
     #for old data
     #Define the source directory
@@ -284,3 +286,12 @@ def old_mb_data():
             export_db(r"\\psg-09\Mikroskop\Images\hsm_db\\" + file)
         except:
             print ('erorr')
+
+
+exp_name = "MB0265"
+dai = "/16dai/"
+#process_and_merge_files("//psg-09/Mikroskop/Exchange/!to_analyze/metadata/MB/" + exp_name + "_meta.csv", "//psg-09/Mikroskop/Images/BluVisionMacro/" + exp_name + dai + exp_name + "_leaf.csv", "plate_index", None)
+#export_db("//psg-09/Mikroskop/Images/hsm_db/" + exp_name + "_db.csv")
+copy_and_verify("//psg-09/Mikroskop/Images/BluVisionMacro/" + exp_name + dai, "//hsm/AGR-BIM/Results/BluVisionMacro/" + exp_name + dai)
+
+
